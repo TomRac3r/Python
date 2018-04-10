e0c71cb4c97fcd5be419b2a641fe22c3a67dc431
Thomas M <marin_t@esme.fr>
2018-04-10 16:39:41 +0100

    Add files via upload

--- DevinettePython.py
+++ DevinettePython.py
@@ -4,23 +4,43 @@
 
 Ceci est un script temporaire.
 """
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
+
+"""Programmer avec Python en s'amusant, Projet 3 'Un jeu de devinette'"""
 import random
 
 #import antigravity
 #import this
 
-mon_invite = 'Propose un nombre : '
-y = random.randint(0, 10)
+def bonjour_random():
+    """Fonction affichant un bonjour random =)"""
+    nb_rand = random.randint(1, 3)
+    if (nb_rand == 1):
+        print('Yo !')
+    elif (nb_rand == 2):
+        print('Wesh alors !')
+    elif (nb_rand == 3):
+        print('Mes salutations camarade !')
 
-while True:
-    nbr_du_joueur = input(mon_invite)
-    if (y == int(nbr_du_joueur)):
-        print("gg mon gral ! t'as trouvé")
-        break
-    elif (int(nbr_du_joueur) > y):
-        print('ton nombre est plus grand ! essaye encore...')
-    elif (int(nbr_du_joueur) < y):
-        print('ton nombre est plus petit...essaye encore !')
+
+def nombre_random() :
+    """Fonction pour faire un nombre random dans l'intervalle [0, 10]"""
+    nb_random = random.randint(0, 10)
+
+
+def devinette_nombre():
+    """Fonction permettant un petit jeu de devinette"""
+    nb_essai = 0
+    while True:
+        nbr_du_joueur = input('Saisi un nombre : ')
+        if (nb_random == int(nbr_du_joueur)):
+            print("GG mon gral ! t'as trouvé")
+            print('Tu as essayé ', nb_essai, ' fois avant de réussir!')
+            break
+        elif (int(nbr_du_joueur) > nb_random):
+            print('Ton nombre est plus grand ! essaye encore...')
+        elif (int(nbr_du_joueur) < nb_random):
+            print('Ton nombre est plus petit...essaye encore !')
+        nb_essai = nb_essai + 1
+        print('Tu es déjà à ton ', nb_essai, ' essai...')
+        
+bonjour_random()        
