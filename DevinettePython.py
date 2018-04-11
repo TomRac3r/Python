12ab8124d0ae9862546293b214c2c8bb020b50f8
Thomas M <marin_t@esme.fr>
2018-04-11 09:19:46 +0100

    Add files via upload

--- DevinettePython.py
+++ DevinettePython.py
@@ -11,6 +11,12 @@ import random
 #import antigravity
 #import this
 
+#################################################################
+#################################################################
+##                      BLOC INTER-LIGNE                       ##
+#################################################################
+#################################################################
+
 def bonjour_random():
     """Fonction affichant un bonjour random parmis 4 choix =)"""
     nb_rand = random.randint(1, 4)
@@ -22,21 +28,35 @@ def bonjour_random():
         print('Mes salutations camarade !')
     elif (nb_rand == 4):
         print('#YOLO')
-
-
+              
+#################################################################
+#################################################################
+##                      BLOC INTER-LIGNE                       ##
+#################################################################
+#################################################################
+        
 def nombre_random() :
     """Fonction pour faire un nombre random dans l'intervalle [0, 10]"""
     nb_random = random.randint(0, 10)
     return nb_random
 
+#################################################################
+#################################################################
+##                      BLOC INTER-LIGNE                       ##
+#################################################################
+#################################################################
 
 def devinette_nombre():
     """Fonction permettant un petit jeu de devinette, on génére un nb_random puis on le compare avec
     un nombre saisi par l'utilisateur (nbr_du_joueur) / On affiche aussi le nb_esai avec un simple
     incrément à chaque tentative."""
+    nb_random = random.randint(0, 10)
     nb_essai = 0
+    INVITE = 'Saisi un nombre : ' 
+#INVITE est en majuscules car c'est une constante, 
+##convention d'écriture :variable=minuscules / CONSTANTES=MAJUSCULES
     while True:
-        nbr_du_joueur = input('Saisi un nombre : ')
+        nbr_du_joueur = input(INVITE)
         if (nb_random == int(nbr_du_joueur)):
             print("GG mon gral ! t'as trouvé")
             print('Tu as essayé ', nb_essai, ' fois avant de réussir!')
@@ -48,6 +68,28 @@ def devinette_nombre():
         nb_essai = nb_essai + 1
         print('Tu es déjà à ton ', nb_essai, ' essai...')
         
-bonjour_random()  
-nombre_random()    
-devinette_nombre()
+#################################################################
+#################################################################
+##                      BLOC INTER-LIGNE                       ##
+#################################################################
+#################################################################      
+         
+def jouer():
+    """Fonction permettant le jeu 'devinette_nombre' avec un pseudo-menu !"""
+    while True :
+        CONTINUER = 'Veux-tu coninuer à jouer ?! 1 pour oui / 2 pour non '
+        saisie_continuer = input(CONTINUER)
+        if (int(saisie_continuer) == 1):
+            print("Et c'est parti pour un tour !")
+            devinette_nombre()
+        elif (int(saisie_continuer) == 2):
+            print('Aurevoir !')
+            break
+        
+#################################################################
+#################################################################
+##                      BLOC INTER-LIGNE                       ##
+#################################################################
+#################################################################
+bonjour_random()
+jouer()
